19c2ae22c906424f01f0394f987ff58124e01a29
Akhilesh Kumar <akleshrailsdev@gmail.com>
2022-11-28 16:47:58 +0530

    Made serilazer dynamic to work with its child classes.

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,30 @@
+# frozen_string_literal: true
+
+# Serializer for required response
 class Serializer
+  class << self
+    attr_reader :attributes
+
+    def attribute(attribute, &block)
+      @attributes ||= {}
+      @attributes[attribute] = block
+    end
+  end
+
+  attr_reader :object
+
+  def initialize(object)
+    @object = object
+  end
+
+  def serialize
+    data = {}
+
+    self.class.attributes.each do |attr, block|
+      data[attr] = object.send(attr)
+      data[attr] = instance_eval(&block) unless block.nil?
+    end
+
+    data.compact
+  end
 end
